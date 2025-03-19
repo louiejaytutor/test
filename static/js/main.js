3984e318fe064beda552f3b9a48414ca80f6aeed
Louie Jay Tutor <109414917+louiejaytutor@users.noreply.github.com>
2025-03-19 11:07:39 +0800

    Add files via upload

--- static/js/main.js
+++ static/js/main.js
@@ -1,28 +1,17 @@
-const pending_btn = document.getElementById("pending-btn");
-const completed_btn = document.getElementById("completed-btn");
-const trash_btn = document.getElementById("trash-btn");
+const task_list = document.getElementById("task-list");
+const add_btn = document.getElementById("add-btn");
 
-const tasks = document.getElementById("tasks");
+for (var i=0; i < 5; i++) {
+    task_list.innerHTML = task_list.innerHTML + `<div onclick="taskInfo(${i})" class="task">
+        <div class="task-title">Dane P. Santiago</div>
+        <div class="task-timestamp">19 Mar 2025 10:34 am</div>
+    </div>`;
+}
 
-const show_task_btn = document.getElementById("show-task-btn");
-const cancel_task_btn = document.getElementById("cancel-task-btn");
-const task_form = document.getElementById("task-form");
-let task_input = document.getElementById("task-input");
+function taskInfo(id) {
+    alert(id);
+}
 
-show_task_btn.addEventListener("click", function() {
-    if (task_form.classList.contains("active")) {
-        show_task_btn.classList.add("active");
-        task_form.classList.remove("active");
-    }
-    else {
-        show_task_btn.classList.remove("active");
-        task_form.classList.add("active");
-    }
-});
-
-cancel_task_btn.addEventListener("click", function() {
-    show_task_btn.classList.add("active");
-    task_form.classList.remove("active");
-    task_form.querySelector("h3").textContent = "Add new task";
-    task_input.value = "";
+add_btn.addEventListener("click", function() {
+    alert("test add button");
 });

40bddbd77a2e7e999d48cd14f7b7f6ca54ce5213
Louie Jay Tutor <109414917+louiejaytutor@users.noreply.github.com>
2025-03-19 11:48:09 +0800

    Add files via upload

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,6 @@
 const task_list = document.getElementById("task-list");
 const add_btn = document.getElementById("add-btn");
 
-for (var i=0; i < 5; i++) {
-    task_list.innerHTML = task_list.innerHTML + `<div onclick="taskInfo(${i})" class="task">
-        <div class="task-title">Dane P. Santiago</div>
-        <div class="task-timestamp">19 Mar 2025 10:34 am</div>
-    </div>`;
-}
-
 function taskInfo(id) {
     alert(id);
 }
